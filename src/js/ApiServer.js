62a032d552ee95a299ad2c3680ee3d9e002f15ef
Kirill Filonchuk <kir.realtor@gmail.com>
2021-07-28 00:11:43 +0300

    commit

--- src/js/ApiServer.js
+++ src/js/ApiServer.js
@@ -1,3 +1,7 @@
+//экспорт массива с объектами жанров и их ID
+import genreMovis from './genre';
+console.log(genreMovis);
+
 const KEY = '494b2b5ea2ae23dbb3e89fabdc88e3f6';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 
@@ -27,9 +31,33 @@ export default class FilmsApiServise {
       })
       .then(data => {
         console.log(data.results);
+        this.giveGenre(data.results[0].genre_ids) //[35, 18]
+        
       });
   }
+// //console.log(data.results[0].genre_ids); получили массив ID жанров
+  
+//   giveGenre(arrayGenre) {
+//     console.log(arrayGenre);
+//     // for (i = 0; i < arrayGenre.length; )
 
+// // function getCommonElements(firstArray, secondArray) {
+// //     const commonElements = [];
+// //     // Пиши код ниже этой строки
+// //   firstArray.forEach((el)=>{
+// //   if (secondArray.includes(el)) {
+// //         commonElements.push(el);
+// //       }
+// //   })
+//     const commonElements = [];
+//     genreMovis.forEach(genreMov)=> {
+//       if (arrayGenre.includes())
+//     }
+
+//     // const genre = genreMovis.reduce((acc, genreMov) => { acc.push(genreMov.name); return acc }, []);
+//     console.log(genre);
+//   }
+  
   fetchFilmsDescription() {
     const url = `${BASE_URL}movie/${this.filmID}?api_key=${KEY}&language=en-US`;
     return fetch(url)
@@ -41,3 +69,5 @@ export default class FilmsApiServise {
       });
   }
 }
+
+
