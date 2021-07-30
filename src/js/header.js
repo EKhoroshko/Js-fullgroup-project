ffe37149d1a896ee0a22194dc7cac3636343b8e0
Alyona Ihnatenko <altaig4@gmail.com>
2021-07-30 14:48:46 +0300

    togle-remake

--- src/js/header.js
+++
@@ -1,7 +0,0 @@
-import header from '../templation/header.hbs';
-import libraryHeader from '../templation/library-header.hbs';
-
-const refs={
-    btnHome:document.querySelector('.js-btnHome'),
-    btnMyLibrary:document.querySelector('.js-btnMyLibrary'),
-};
