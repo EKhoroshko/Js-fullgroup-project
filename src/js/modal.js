038f4520625c0bb6dc258efc4dcae6b174a969ef
Valentina Mingalieva <valentina.mingalieva@gmail.com>
2021-08-02 21:38:31 +0200

    clean

--- src/js/modal.js
+++ src/js/modal.js
@@ -144,28 +144,3 @@ const deleteFilm = id => {
     renderCardMain(arrayUpdateFilms);
   }
 };
-
-// END NEW
-// scroll
-/* const disableScroll = () => {
-  const widthScroll = window.innerWidth - document.body.offsetWidth;
-  document.body.dbScrollY = window.scrollY;
-  document.body.style.cssText = `
-        position: fixed;
-        top: ${-window.scrollY}px;
-        left: 0;
-        width: 100%;
-        height: 100vh;
-        overflow: hidden;
-        padding-right: ${widthScroll}px;
-    `;
-};
-
-const enableScroll = () => {
-  document.body.style.cssText = '';
-  window.scroll({
-    top: document.body.dbScrollY,
-  });
-}; */
-
-/* export { disableScroll, enableScroll }; */
--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -36,26 +36,3 @@ refs.footerLink.addEventListener('click', onOpenTeamModal);
 refs.closeTeamModalBtn.addEventListener('click', onCloseTeamModal);
 refs.leaveBtn.addEventListener('click', onCloseTeamModal);
 refs.teamModalBackdrop.addEventListener('click', onCloseTeamModal);
-
-// scroll
-
-/* const disableScroll = () => {
-  const widthScroll = window.innerWidth - document.body.offsetWidth;
-  document.body.dbScrollY = window.scrollY;
-  document.body.style.cssText = `
-        position: fixed;
-        top: ${-window.scrollY}px;
-        left: 0;
-        width: 100%;
-        height: 100vh;
-        overflow: hidden;
-        padding-right: ${widthScroll}px;
-    `;
-};
-
-const enableScroll = () => {
-  document.body.style.cssText = '';
-  window.scroll({
-    top: document.body.dbScrollY,
-  });
-}; */
