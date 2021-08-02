d2d237edad738b437cce2ea7832f50e99bdf09b3
yevhensmirnov <76063173+yevhensmirnov@users.noreply.github.com>
2021-08-02 22:55:13 +0300

    добавил плавный скролл

--- src/js/scrollup.js
+++ src/js/scrollup.js
@@ -10,15 +10,30 @@ window.onload = function() { // после загрузки страницы
     scrollUp.onmouseout = function() { //убрать прозрачность
       scrollUp.style.opacity = 0.5;
       scrollUp.style.filter  = 'alpha(opacity=20)';
-    };
+  };
   
-    scrollUp.onclick = function() { //обработка клика
-      window.scrollTo(0,0);
-      window.scrollBy({ behavior: 'smooth'});
+  var scrolled;
+  var timer;
+
+  
+  scrollUp.onclick = function () {
+       scrolled= window.pageYOffset //обработка клика
+      //window.scrollTo(0,0);
+    scrollToTop();
       
 
     };
-  
+  function scrollToTop() {
+    if (scrolled > 0) {
+      window.scrollTo(0, scrolled);
+      scrolled = scrolled - 50;
+      timer = setTimeout(scrollToTop, 20);
+    }
+    else {
+      clearTimeout(timer);
+      window.scrollTo(0,0)
+    }
+  }
   // show button
   
     window.onscroll = function () { // при скролле показывать и прятать блок
